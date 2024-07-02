e53d861878be212e619a1554dc0190075510456c
Rehan <77270784+rehanshami@users.noreply.github.com>
2024-07-03 00:52:23 +1200

    Create demo web app

---
+++ src/sections/CustomerReviews.jsx
@@ -0,0 +1,24 @@
+import ReviewCard from "../components/ReviewCard"
+import { reviews } from "../constants"
+
+const CustomerReviews = () => {
+  return (
+    <section className="max-container">
+      <h3 className="font-palanquin text-center text-4xl font-bold"> What our <span className="text-coral-red ">customers</span> say</h3>
+      <p className="info-text m-auto mt-4 max-w-lg text-center">Hear genuine stories from our statisfied customers</p>
+      <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+        {reviews.map((review, index) => (
+          <ReviewCard 
+          key={index}
+          imgURL={review.imgURL}
+          customerName={review.customerName}
+          rating={review.rating}
+          feedback={review.feedback}
+          />
+        ))}
+      </div>
+    </section>
+  )
+}
+
+export default CustomerReviews
